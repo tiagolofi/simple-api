bc2808b8803374bcfd0262a53d4ddd4afdafb5e6
tiagolofi <jejeco75@gmail.com>
2025-12-03 09:17:06 -0300

    teste

--- sapi/core/__init__.py
+++ sapi/core/__init__.py
@@ -1,5 +1,5 @@
 
-from core.app import SimpleApi
-from core.managers import Managers, ManagerYaml, Manager
+from app import SimpleApi
+from managers import Managers, ManagerYaml, Manager
 from models import Request, Response
 from router import Router 
