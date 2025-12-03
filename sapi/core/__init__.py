51e9cbd7e6fb2fab4a3c07786badcb5db284982e
tiagolofi <jejeco75@gmail.com>
2025-12-03 09:21:24 -0300

    correção

--- sapi/core/__init__.py
+++ sapi/core/__init__.py
@@ -1,2 +1,9 @@
 
-from core import SimpleApi, Managers, ManagerYaml, Manager, Request, Response, Router
+from .app import SimpleApi
+from .managers import Managers, ManagerYaml, Manager
+from .models import Request, Response
+from .router import Router 
+
+__all__ = [
+    "SimpleApi", "Managers", "ManagerYaml", "Manager", "Request", "Response", "Router"
+]
